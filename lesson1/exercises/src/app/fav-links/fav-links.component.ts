f8d41f241bf633bd348a2dc6e72af144c6927aeb
Grant Crawford <grantttt.crawford@gmail.com>
2022-12-05 19:30:42 -0600

    finished lesson1

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks = ['https://www.reddit.com', 'https://www.angular.io']
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
